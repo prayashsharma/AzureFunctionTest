73ed7c8cdf8b3cb8e3a76b7b44f43e2ecc1fe194
prayashsharma <prayashsharma@live.com>
2019-09-17 11:06:57 -0500

    Add project files.

---
+++ MainFunction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MainFunction
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! from MainFunction");
+        }
+    }
+}
---
+++ MainFunction/run.ps1
@@ -0,0 +1,1 @@
+﻿.\MainFunction.exe
